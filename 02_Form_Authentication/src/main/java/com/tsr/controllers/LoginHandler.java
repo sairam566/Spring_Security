c79965b4a8a47a9ffb78a089dcd85efe088e9b0a
sairam566 <sairam1431995@gmail.com>
2024-08-06 10:53:18 +0530

    Handler

---
+++ 02_Form_Authentication/src/main/java/com/tsr/controllers/LoginHandler.java
@@ -0,0 +1,18 @@
+package com.tsr.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginHandler {
+
+	@GetMapping("/login.htm")
+	public String login() {
+		return "loginform";
+	}
+	
+	@GetMapping("/logout.htm")
+	public String logout() {
+		return "logoutform";
+	}
+}
